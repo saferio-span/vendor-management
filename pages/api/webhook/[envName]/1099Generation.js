207e7faace8ecac3af7807e00b0c95046a5029fa
saferio-span <stsjohnsaferio@dotnetethics.com>
2021-12-29 22:23:17 +0530

    Changed functionality for 1099 webhook

--- pages/api/webhook/[envName]/1099Generation.js
+++ pages/api/webhook/[envName]/1099Generation.js
@@ -21,50 +21,84 @@ export default async function handler(req,res)
     const taxYear = req.body.TaxYear
     const form1099NECRecords = req.body.Form1099NECRecords
 	// try {
-        form1099NECRecords.forEach((record)=>{
+        form1099NECRecords.forEach(async(record)=>{
             console.log(`Submission Id : ${submissionId}`)
             console.log(`Record Id : ${record.RecordId}`)
-            Records1099.find({
+            const availableHook = await Records1099.find({
                 SubmissionId:submissionId,
                 RecordId: record.RecordId
-            },(err,data)=>{
-                if (err){
-                    console.log(err)
-                    return res.status(401).send('Error occured in checking that the record already exist or not.');
-                }
-                else
-                {
-                    if(data.length==0)
-                    {
-                        console.log(`Record not found creating a new one for ${record.RecordId}`)
-                        const new1099Response = new Records1099({
-                            SubmissionId: submissionId,
-                            BusinessId:businessId,
-                            PayerRef:payerRef,
-                            TaxYear:taxYear,
-                            RecordId: record.RecordId,
-                            RecipientId: record.RecipientId,
-                            PayeeRef:record.PayeeRef,
-                            NECBox1:record.NecBox1,
-                            NECBox4:record.NecBox4,
-                            FederalReturnStatus: record.FederalReturn.Status,
-                            FederalReturnStatusTs: record.FederalReturn.StatusTs,
-                            FederalReturnInfo: record.FederalReturn.Info,
-                            environment:envName
-                        });
-                        new1099Response.save();
-                        console.log(`generated Successfully for ${record.RecordId}`)
-                    }
-                    else
-                    {
-                        Records1099.findOneAndUpdate({RecordId:record.RecordId},{
-                            FederalReturnStatus: record.FederalReturn.Status,
-                            FederalReturnStatusTs: record.FederalReturn.StatusTs,
-                            FederalReturnInfo: record.FederalReturn.Info
-                        })
-                    }
-                  } 
             })
+
+            console.log(`Records already available start ------------------`)
+            console.log(availableHook)
+            console.log(`Records already available end --------------------`)
+
+            if(availableHook.length == 0)
+            {
+                console.log(`Record not found creating a new one for ${record.RecordId}`)
+                const new1099Response = new Records1099({
+                    SubmissionId: submissionId,
+                    BusinessId:businessId,
+                    PayerRef:payerRef,
+                    TaxYear:taxYear,
+                    RecordId: record.RecordId,
+                    RecipientId: record.RecipientId,
+                    PayeeRef:record.PayeeRef,
+                    NECBox1:record.NecBox1,
+                    NECBox4:record.NecBox4,
+                    FederalReturnStatus: record.FederalReturn.Status,
+                    FederalReturnStatusTs: record.FederalReturn.StatusTs,
+                    FederalReturnInfo: record.FederalReturn.Info,
+                    environment:envName
+                });
+                await new1099Response.save();
+            }
+            else
+            {
+                await Records1099.findOneAndUpdate({RecordId:record.RecordId},{
+                    FederalReturnStatus: record.FederalReturn.Status,
+                    FederalReturnStatusTs: record.FederalReturn.StatusTs,
+                    FederalReturnInfo: record.FederalReturn.Info
+                })
+            }
+        //     ,(err,data)=>{
+        //         if (err){
+        //             console.log(err)
+        //             return res.status(401).send('Error occured in checking that the record already exist or not.');
+        //         }
+        //         else
+        //         {
+        //             if(data.length==0)
+        //             {
+                        // console.log(`Record not found creating a new one for ${record.RecordId}`)
+                        // const new1099Response = new Records1099({
+                        //     SubmissionId: submissionId,
+                        //     BusinessId:businessId,
+                        //     PayerRef:payerRef,
+                        //     TaxYear:taxYear,
+                        //     RecordId: record.RecordId,
+                        //     RecipientId: record.RecipientId,
+                        //     PayeeRef:record.PayeeRef,
+                        //     NECBox1:record.NecBox1,
+                        //     NECBox4:record.NecBox4,
+                        //     FederalReturnStatus: record.FederalReturn.Status,
+                        //     FederalReturnStatusTs: record.FederalReturn.StatusTs,
+                        //     FederalReturnInfo: record.FederalReturn.Info,
+                        //     environment:envName
+                        // });
+                        // new1099Response.save();
+        //                 // console.log(`generated Successfully for ${record.RecordId}`)
+        //             }
+        //             else
+        //             {
+                        // Records1099.findOneAndUpdate({RecordId:record.RecordId},{
+                        //     FederalReturnStatus: record.FederalReturn.Status,
+                        //     FederalReturnStatusTs: record.FederalReturn.StatusTs,
+                        //     FederalReturnInfo: record.FederalReturn.Info
+                        // })
+        //             }
+        //           } 
+        //     })
         })
 
 		res.status(200).send();
