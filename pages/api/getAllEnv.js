5f1e31bc9b3e56b6908aa2315b80f255a53639a1
saferio-span <90183742+saferio-span@users.noreply.github.com>
2022-07-21 00:49:39 +0530

    Update getAllEnv.js

--- pages/api/getAllEnv.js
+++ pages/api/getAllEnv.js
@@ -7,8 +7,17 @@ export default async function handler(req,res)
 {
 	const email =  req.body.email
 	try {
-		const env = await Environment.find({email:email}).sort({ name: 'asc' }).limit(100);
-		res.status(200).json(env);
+		if(email !== null)
+		{
+			const env = await Environment.find({email:email}).sort({ name: 'asc' });
+			res.status(200).json(env);
+		}
+		else
+		{
+			const env = await Environment.find().sort({ name: 'asc' });
+			res.status(200).json(env);
+		}
+		
 	} catch (err) {
 		console.error(err);
 		res.status(500).send(err);
